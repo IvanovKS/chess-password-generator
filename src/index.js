5d8fc4bc93c1bf4ec9d5a24add512ca497d81995
IvanovKS <Skitchi@mail.ru>
2023-09-26 20:52:49 +0300

    feat: add password copy function

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import getPassword from './modules/generator';
 const CHECKBOX = document.querySelectorAll('input[type="checkbox"]');
 const PASSWORD_VALUE = document.querySelector('.password-value p');
 const RESET = document.querySelector('.password-value button');
+const COPY = document.querySelector('.copy');
+const COPIED = document.querySelector('.generator-value img');
 
 RANGE.addEventListener('change', () => {
   PASSWORD_VALUE.textContent = getPassword(RANGE.value);
@@ -20,4 +22,17 @@ CHECKBOX.forEach((el) => {
 
 RESET.addEventListener('click', () => {
   PASSWORD_VALUE.textContent = getPassword(RANGE.value);
+});
+
+COPY.addEventListener('click', () => {
+  const textToCopy = PASSWORD_VALUE.textContent;
+  navigator.clipboard.writeText(textToCopy);
+  if (PASSWORD_VALUE.textContent !== '') {
+    COPY.style.display = 'none';
+    COPIED.style.display = 'block';
+    setTimeout(() => {
+      COPIED.style.display = 'none';
+      COPY.style.display = '';
+    }, 1500);
+  }
 });
